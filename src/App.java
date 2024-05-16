ad5191ce997f99b8353fb05f4429add97d27e605
casey <cpmoden@gmail.com>
2024-05-16 10:13:30 -0400

    update

--- src/App.java
+++ src/App.java
@@ -1,7 +1,16 @@
 public class App {
-	public static void main(String[] args)
+	public static void main(String[] args){
+		int[][] a = {
+			{-5,-2,-3,7},
+			{1,-5,-2,2},
+			{1,-2,3,-4}
+		};
+		System.out.println(sumColumn(a,1)); //should print -9
+		System.out.println(sumColumn(a,3)); //should print 5
+	}
+	public static int sumColumn(int[][] array, int column)
 	{
-		
+		return -1; //placeholder
 	}
 	
 }
